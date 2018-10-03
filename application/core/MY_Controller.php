8955f534da36c2f5feb5f6ea96d93e37b7dc5ffe
Éderson Tiago Szlachta <ederson@pc1.edtsz.com>
2018-10-02 23:25:38 -0300

    Controle de acesso por nível no CORE da aplicação.

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,15 +1,17 @@
 <?php
 
-class MY_ControllerLogado extends CI_Controller{
+class MY_Controller extends CI_Controller{
   function __construct(){
     parent:: __construct();
 
     if (! $this->session->userdata('logado')){
       redirect('/login');
     }
+
+    $usuario = $this->session->userdata('usuario');
+
+    if ($this->uri->segment(1) !== $usuario['contexto']){
+		die($this->load->view('aviso_permissao',NULL,TRUE));
+    }
   }
-
-
-
-
 }
